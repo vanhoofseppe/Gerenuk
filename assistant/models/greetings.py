d4dcadc6e2df97c636f29315726b336f1af37b8b
Seppe Vanhoof <vanhoofseppe@gmail.com>
2017-10-08 16:47:57 +0200

    delete id from greetings

--- assistant/models/greetings.py
+++ assistant/models/greetings.py
@@ -1,11 +1,9 @@
 from django.db import models
 from .information import Information
-import uuid
 
 
 # Create your models here.
 class Greeting(models.Model):
-    id = models.UUIDField('id', primary_key=True, default=uuid.uuid4, editable=False)
     when = models.DateTimeField('date created', auto_now_add=True)
 
 
